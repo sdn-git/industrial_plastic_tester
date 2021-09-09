863afdd7a4ebffd8279c6690939f511259782797
sdn-git <77998105+sdn-git@users.noreply.github.com>
2021-09-09 20:18:01 +0300

    Delete AvrBuild.bat

--- Soft/AvrBuild.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"E:\Program Files\Atmel\AvrAssembler2\avrasm2.exe" -S "E:\Projects\FastexChecker\Soft\labels.tmp" -fI -W+ie -o "E:\Projects\FastexChecker\Soft\Project2.hex" -d "E:\Projects\FastexChecker\Soft\Project2.obj" -e "E:\Projects\FastexChecker\Soft\Project2.eep" -m "E:\Projects\FastexChecker\Soft\Project2.map" "E:\Projects\FastexChecker\Soft\Project2.asm"
