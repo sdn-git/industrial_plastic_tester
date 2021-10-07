a04337c22b312ca5ce87157314e1cd697b374d83
Dmitrii Solomitckii <77998105+sdn-git@users.noreply.github.com>
2021-10-08 00:31:08 +0300

    Update LEDPause_Flashing.asm

--- LEDPause_Flashing.asm
+++ LEDPause_Flashing.asm
@@ -1,36 +1,42 @@
+;/// LED flashing modes
 LEDPause_Flashing:
-Push_All
-clr tmp
-clr tmp3
-clr Xtmp
-lds	  xtmp,S_FlagButt1			    	;Это есть обработка паузы. Когда м нажимаем кнопку S_PauseButt инкремен
-sbrs  xtmp,f_PauseSetted		        ;кнопка еще не нажималась.А пауза включена?
-rjmp  NoFlashing
-		lds		tmp,S_LedFlashing1
-		lds		xtmp,S_LedFlashing2
-		inc tmp
-		cpi tmp,0xff
-		brne	NoFlashing
-		clr tmp
-		inc	xtmp
-		cpi	xtmp,0x03
-		brne	NoFlashing
-YesFlashing:
-		CLR XTMP
-		clr tmp
-		lds		TMP3,S_LK4			;выгружаем ячейку в тмп\
-		sbrs    tmp3,f_L2
-		rjmp	OnPauseLed
-		rjmp	OffPauseLed
+	Push_All
+	clr	tmp
+	clr	tmp3
+	clr	Xtmp
+	lds	xtmp,S_FlagButt1			    	
+	sbrs	xtmp,f_PauseSetted		        
+	rjmp	NoFlashing
+	lds 	tmp,S_LedFlashing1
+	lds	xtmp,S_LedFlashing2
+	inc	tmp
+	cpi	tmp,0xff
+	brne	NoFlashing
+	clr	tmp
+	inc	xtmp
+	cpi	xtmp,0x03
+	brne	NoFlashing
+
+YesFlashing:    ;/// blinking
+	CLR 	XTMP
+	clr 	tmp
+	lds	TMP3,S_LK4			
+	sbrs    tmp3,f_L2
+	rjmp	OnPauseLed
+	rjmp	OffPauseLed
+	
 OnPauseLed:
-		sbr		tmp3,m_l2
-		rjmp	BackToRam	
+	sbr	tmp3,m_l2
+	rjmp	BackToRam
+	
 OffPauseLed:
-		cbr		tmp3,m_l2
+	cbr	tmp3,m_l2
+	
 BackToRam:
-		sts		S_LK4,Tmp3
-NoFlashing:
-sts		S_LedFlashing1,Tmp
-sts		S_LedFlashing2,xtmp	
-Pop_All
+	sts	S_LK4,Tmp3
+	
+NoFlashing:     /// constant
+	sts	S_LedFlashing1,Tmp
+	sts	S_LedFlashing2,xtmp	
+	Pop_All
 ret
