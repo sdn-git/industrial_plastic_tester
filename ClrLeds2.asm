8a36b7bf741eca64f8eb43dc77b74d88b72a5a2e
Dmitrii Solomitckii <77998105+sdn-git@users.noreply.github.com>
2021-10-07 23:37:44 +0300

    Update ClrLeds2.asm

--- ClrLeds2.asm
+++ ClrLeds2.asm
@@ -1,26 +1,27 @@
-ClrLeds2: ;Все то же самое, только затираем СД воторй группы.
-Push_ALL
-cli
-lds tmp3,S_LK0
-sbr tmp3,F_k1
-cbr tmp3,M_L3
-sts S_LK0,tmp3
+;\\\ Clear leds of the second buttons group
+ClrLeds2: 
+  Push_ALL
+  cli
+  lds tmp3,S_LK0
+  sbr tmp3,F_k1
+  cbr tmp3,M_L3
+  sts S_LK0,tmp3
 
-lds tmp3,S_LK1
-sbr tmp3,F_k1
-cbr tmp3,M_L3
-sts S_LK1,tmp3
+  lds tmp3,S_LK1
+  sbr tmp3,F_k1
+  cbr tmp3,M_L3
+  sts S_LK1,tmp3
 
-lds tmp3,S_LK2
-sbr tmp3,F_k1
-cbr tmp3,M_L3
-sts S_LK2,tmp3
+  lds tmp3,S_LK2
+  sbr tmp3,F_k1
+  cbr tmp3,M_L3
+  sts S_LK2,tmp3
 
-lds tmp3,S_LK3
-sbr tmp3,F_k1
-cbr tmp3,M_L3
-sts S_LK3,tmp3
+  lds tmp3,S_LK3
+  sbr tmp3,F_k1
+  cbr tmp3,M_L3
+  sts S_LK3,tmp3
 
-sei
-POP_All
+  sei
+  POP_All
 ret
