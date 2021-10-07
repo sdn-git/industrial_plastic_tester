ef2868a7c2cf5a92e8892df1c4345f194319ada7
Dmitrii Solomitckii <77998105+sdn-git@users.noreply.github.com>
2021-10-08 01:09:27 +0300

    Update WriteEEPROM.asm

--- WriteEEPROM.asm
+++ WriteEEPROM.asm
@@ -1,55 +1,58 @@
 WriteConstOpenCloseInEEPROM:
-cli
-Push_All
+	cli
+	Push_All
+	
 WriteLowConstOpenClose:
 	sbic	EECR,EEWE
 	rjmp	WriteLowConstOpenClose
-	ldi		tmp,E_OpenCloseAddrL		
-	ldi		tmp2,0
-	out		EEARH,tmp2
-	out		EEARL,tmp
-	out		EEDR,XL
-	sbi		EECR,EEMWE
-	sbi		EECR,EEWE
+	ldi	tmp,E_OpenCloseAddrL		
+	ldi	tmp2,0
+	out	EEARH,tmp2
+	out	EEARL,tmp
+	out	EEDR,XL
+	sbi	EECR,EEMWE
+	sbi	EECR,EEWE
 
 WriteHighConstOpenClose:
 	sbic	EECR,EEWE
 	rjmp	WriteHighConstOpenClose
 	ldi 	tmp,E_OpenCloseAddrH
-	ldi		tmp2,0
-	out		EEARH,tmp2
-	out		EEARL,tmp
-	out		EEDR,XH
-	sbi		EECR,EEMWE
-	sbi		EECR,EEWE
+	ldi	tmp2,0
+	out	EEARH,tmp2
+	out	EEARL,tmp
+	out	EEDR,XH
+	sbi	EECR,EEMWE
+	sbi	EECR,EEWE
+	
 Pop_All
-sei
+	sei
 	ret
 
-
 WriteConstLengthInEEPROM:
-cli
-Push_All
+	cli
+	Push_All
+	
 WriteLowConstLength:
 	sbic	EECR,EEWE
 	rjmp	WriteLowConstLength
-	ldi		tmp,E_LengthAddrL 		
-	ldi		tmp2,0
-	out		EEARH,tmp2
-	out		EEARL,tmp
-	out		EEDR,XL
-	sbi		EECR,EEMWE
-	sbi		EECR,EEWE
+	ldi	tmp,E_LengthAddrL 		
+	ldi	tmp2,0
+	out	EEARH,tmp2
+	out	EEARL,tmp
+	out	EEDR,XL
+	sbi	EECR,EEMWE
+	sbi	EECR,EEWE
+	
 WriteHighConstLength:
 	sbic	EECR,EEWE
 	rjmp	WriteHighConstLength
 	ldi 	tmp,E_LengthAddrH 
-	ldi		tmp2,0
-	out		EEARH,tmp2
-	out		EEARL,tmp
-	out		EEDR,XH
-	sbi		EECR,EEMWE
-	sbi		EECR,EEWE
-Pop_All
-sei
+	ldi	tmp2,0
+	out	EEARH,tmp2
+	out	EEARL,tmp
+	out	EEDR,XH
+	sbi	EECR,EEMWE
+	sbi	EECR,EEWE
+	Pop_All
+	sei
 	ret
