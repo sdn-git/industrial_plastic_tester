8077cee445bc51a9595ddbf5e7a346d06f411329
Dmitrii Solomitckii <77998105+sdn-git@users.noreply.github.com>
2021-10-08 23:21:09 +0300

    Update ReadyIndicate.asm

--- ReadyIndicate.asm
+++ ReadyIndicate.asm
@@ -1,14 +1,14 @@
 ReadyIndicate:
-push_all
-ldi tmp, _spc
-sts S_D1,tmp
-ldi tmp, _Eb
-sts S_D2,tmp
-ldi tmp,_Ns
-sts S_D3,tmp
-ldi tmp,_Ob
-sts S_D4,tmp
-ldi tmp,_Ds
-sts S_D5,tmp
-pop_all
+        push_all
+        ldi tmp, _spc
+        sts S_D1,tmp
+        ldi tmp, _Eb
+        sts S_D2,tmp
+        ldi tmp,_Ns
+        sts S_D3,tmp
+        ldi tmp,_Ob
+        sts S_D4,tmp
+        ldi tmp,_Ds
+        sts S_D5,tmp
+        pop_all
 ret
